bc867cf758095e92de8a6f3a37edc590ffcd6ba0
Vedangi05 <vedangiddeshpande@gmail.com>
2025-10-04 00:40:20 -0500

    Updaated the API link from Localhost to Deployed ink

--- gita-story-frontend/app/page.tsx
+++ gita-story-frontend/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/story", {
+      const res = await fetch("http://127.0.0.1:5000/story", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ quote }),
