0633374c25293000c022459b50d9e0fbaf16739b
Vedangi05 <vedangiddeshpande@gmail.com>
2025-10-04 00:49:40 -0500

    crrected deployment link

--- gita-story-frontend/app/page.tsx
+++ gita-story-frontend/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:5000/story", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/story`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ quote }),
