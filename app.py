040883156f5d4e72965a199978d094da4c4b9d1c
Vedangi05 <vedangiddeshpande@gmail.com>
2025-10-07 22:25:28 -0500

    Updated port number

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from agent import StoryAgent
+import os
 
 app = Flask(__name__)
 CORS(app)  # allow frontend requests
@@ -21,4 +22,6 @@ def generate_story():
         return jsonify({"story": f"⚠️ Error: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    # app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
